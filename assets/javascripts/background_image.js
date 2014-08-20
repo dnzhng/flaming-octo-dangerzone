c266364d9c8f39cae82c4e491d7da31a92589cc2
Daniel Zhang <dyz2@duke.edu>
2014-08-20 01:10:38 -0700

    Set backgrounds better, include top sites now

--- assets/javascripts/background_image.js
+++
@@ -1,4 +0,0 @@
-$.getJSON("http://www.reddit.com/r/wallpapers/hot.json?sort=new", function(response) {
-  imageUrl = response.data.children[0].data.url;
-  $('body').css("background-image", "url("+ imageUrl + ")");
-})
---
+++ assets/javascripts/newTab.js
@@ -0,0 +1,19 @@
+$.getJSON("http://www.reddit.com/r/wallpapers/hot.json?sort=new", function(response) {
+  imageUrl = response.data.children[0].data.url;
+  $('body').css("background-image", "url("+ imageUrl + ")");
+})
+
+function buildPopupDom(mostVisitedURLs) {
+  var popupDiv = document.getElementById('most_visited');
+
+  for (var i = 0; i < 5; i++) {
+    var a = popupDiv.appendChild(document.createElement('a'));
+    a.href = mostVisitedURLs[i].url;
+    a.appendChild(document.createTextNode(mostVisitedURLs[i].title.match(/[a-zA-Z]+/)));
+  }
+}
+
+
+window.onload = function() {
+  chrome.topSites.get(buildPopupDom);
+}
