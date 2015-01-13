7023435625eb8ac6046302e4145bd2c146f20217
Dan Zhang <dyz2@duke.edu>
2015-01-13 17:26:57 -0500

    Filter out non image results in API response

--- assets/javascripts/newTab.js
+++ assets/javascripts/newTab.js
@@ -1,6 +1,6 @@
 $.getJSON("http://www.reddit.com/r/EarthPorn/hot.json?sort=new", function(response) {
   var n = Math.round(Math.random()*5)+1;
-  imageUrl = response.data.children[n].data.url;
+  imageUrl = response.data.children.filter(isImage)[n].data.url;
   $('html').css("background-image", "url("+ imageUrl + ")");
 })
 
@@ -12,6 +12,14 @@ function buildPopupDom(mostVisitedURLs) {
     a.href = mostVisitedURLs[i].url;
     a.appendChild(document.createTextNode(mostVisitedURLs[i].title.match(/[a-zA-Z]+/)));
   }
+}
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+function isImage(link) {
+  return link.data.url.endsWith("jpg") || link.data.url.endsWith("png");
 }
 
 
